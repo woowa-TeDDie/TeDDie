84163b1e10499f0f13a75f7c6aedb790e97e3862
jyt6640 <jyt6640@gmail.com>
2025-11-06 16:36:55 +0900

    refactor(RequestBodyBuilder): 하드코딩 된 문자열 상수 처리

--- src/main/java/TeDDie/api/RequestBodyBuilder.java
+++ src/main/java/TeDDie/api/RequestBodyBuilder.java
@@ -3,13 +3,18 @@ package TeDDie.api;
 import java.util.List;
 
 public class RequestBodyBuilder {
+    private static final String DEFAULT_MODEL = "a.x-4.0-light";
+    private static final String USER_ROLE = "user";
+    private static final double DEFAULT_TEMPERATURE = 0.7;
+    private static final int DEFAULT_MAX_TOKENS = 2000;
+
     public ApiRequest buildRequestObject(String prompt) {
-        ApiMessage message = new ApiMessage("user", prompt);
+        ApiMessage message = new ApiMessage(USER_ROLE, prompt);
         return new ApiRequest(
-                "a.x-4.0-light",
+                DEFAULT_MODEL,
                 List.of(message),
-                0.7,
-                2000
+                DEFAULT_TEMPERATURE,
+                DEFAULT_MAX_TOKENS
         );
     }
 }
