45cb7b9781849aec86faad065ac9500a0560b4f2
jyt6640 <jyt6640@gmail.com>
2025-11-15 10:41:57 +0900

    chore: 프로젝트 자동 생성을 위한 프로젝트 템플릿 추가

---
+++ src/main/resources/template/src/main/java/Application.java
@@ -0,0 +1,7 @@
+package {{PACKAGE_NAME}};
+
+public class Application {
+    public static void main(String[] args) {
+        // TODO: 프로그램 구현
+    }
+}
---
+++ src/main/resources/template/src/test/java/ApplicationTest.java
@@ -0,0 +1,26 @@
+package {{PACKAGE_NAME}};
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ApplicationTest extends NsTest {
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
+    @Test
+    void 기능_테스트() {
+        
+    }
+
+    @Test
+    void 예외_테스트() {
+        
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
