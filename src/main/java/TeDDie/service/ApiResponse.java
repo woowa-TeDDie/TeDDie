a1162395d48e9a1c81c5baf656d0f6ca6b4fb19d
jyt6640 <jyt6640@gmail.com>
2025-11-15 21:32:30 +0900

    feat(ApiResponse): choice가 비어있으면 예외 발생 기능 구현

--- src/main/java/TeDDie/service/ApiResponse.java
+++ src/main/java/TeDDie/service/ApiResponse.java
@@ -2,8 +2,27 @@ package TeDDie.service;
 
 import java.util.List;
 
-record ApiResponse(List<Choice> choices) {}
+record ApiResponse(List<Choice> choices) {
+    String extractResponse() {
+        validateEmpty();
+        return choices.get(0).extractMessage();
+    }
 
-record Choice(Message message) {}
+    private void validateEmpty() {
+        if (choices == null || choices.isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] API 응답에 choice가 없습니다.");
+        }
+    }
+}
 
-record Message(String content) {}
+record Choice(Message message) {
+    String extractMessage() {
+        return message.extractContent();
+    }
+}
+
+record Message(String content) {
+    String extractContent() {
+        return content;
+    }
+}
