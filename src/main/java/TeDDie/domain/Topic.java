ad9d92e30f54a977f8f387811c4a9ab8f0cd90f8
jyt6640 <jyt6640@gmail.com>
2025-11-16 17:09:58 +0900

    test(TopicTest): 주제가 빈 입력값 일 때 예외 발생 테스트 추가

--- src/main/java/TeDDie/domain/Topic.java
+++ src/main/java/TeDDie/domain/Topic.java
@@ -4,6 +4,13 @@ public class Topic {
     private final String value;
 
     public Topic(String value) {
+        validateEmptyInput(value);
         this.value = value;
+    }
+
+    private void validateEmptyInput(String value) {
+        if(value == null || value.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 주제는 빈 값일 수 없습니다.");
+        }
     }
 }
--- src/test/java/TeDDie/domain/TopicTest.java
+++ src/test/java/TeDDie/domain/TopicTest.java
@@ -1,6 +1,7 @@
 package TeDDie.domain;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,5 +16,17 @@ public class TopicTest {
         //when&then
         assertThatCode(() -> new Topic(input))
                 .doesNotThrowAnyException();
+    }
+
+    @DisplayName("주제가 빈 입력값 일 때 예외 발생")
+    @Test
+    void 주제가_빈_입력값_일_때_예외_발생() {
+        //given
+        String input = "";
+
+        //when&than
+        assertThatThrownBy(() -> new Topic(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 주제는 빈 값일 수 없습니다.");
     }
 }
