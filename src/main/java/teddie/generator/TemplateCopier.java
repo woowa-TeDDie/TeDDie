c76c76e250b06a82828fd44dfa4f420acd7ee828
jyt6640 <jyt6640@gmail.com>
2025-11-17 14:23:19 +0900

    refactor(TemplateCopier): depth를 2 이하로 유지

--- src/main/java/teddie/generator/TemplateCopier.java
+++ src/main/java/teddie/generator/TemplateCopier.java
@@ -17,16 +17,12 @@ public class TemplateCopier {
     }
 
     private void copyFile(Path source, Path templatePath, Path targetPath) {
-        try {
-            Path destination = targetPath.resolve(templatePath.relativize(source));
-            if (Files.isDirectory(source)) {
-                Files.createDirectories(destination);
-                return;
-            }
-            Files.copy(source, destination, StandardCopyOption.REPLACE_EXISTING);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        Path destination = targetPath.resolve(templatePath.relativize(source));
+        if (Files.isDirectory(source)) {
+            createDirectories(destination);
+            return;
         }
+        copyRegularFile(source, targetPath);
     }
 
     private Path getTemplatePath() {
@@ -34,6 +30,22 @@ public class TemplateCopier {
             return Path.of(getClass().getResource(TEMPLATE_PATH).toURI());
         } catch (Exception e) {
             throw new RuntimeException("[ERROR] 템플릿 경로를 찾을 수 없습니다: " + e.getMessage(), e);
+        }
+    }
+
+    private void createDirectories(Path destination) {
+        try {
+            Files.createDirectories(destination);
+        } catch (IOException e) {
+            throw new RuntimeException("[ERROR] 디렉토리가 생성되지 않았습니다.");
+        }
+    }
+
+    private void copyRegularFile(Path source, Path targetPath) {
+        try {
+            Files.copy(source, targetPath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            throw new RuntimeException("[ERROR] 디렉토리가 생성되지 않았습니다.");
         }
     }
 }
